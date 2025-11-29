bd98a7c5afa52e45fde3e5a1b8bc9a2c124b967a
MB <bhatgare.mayank10@gmail.com>
2025-11-30 03:36:42 +0530

    chore: cleanup .open-next

--- .open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"NEXT_PUBLIC_API_URL":"https://api.dagent.dev"};
-export const development = {"NEXT_PUBLIC_API_URL":"https://api.dagent.dev"};
-export const test = {"NEXT_PUBLIC_API_URL":"https://api.dagent.dev"};
